5c678994a88f71067bc3a4c575045bd1db309685
kurosio <harborwolf1@gmail.com>
2022-12-25 01:35:23 +0700

    corrects windows elements

--- src/game/client/components/windows.h
+++ src/game/client/components/windows.h
@@ -2,34 +2,39 @@
 #define GAME_CLIENT_COMPONENTS_WINDOWS_H
 #include <game/client/component.h>
 
-#include <engine/textrender.h>
-
 /*
-	Windows GUI elements are placed between CMenus and CWindows
-	Because Teeworlds does not support a layer-by-layer UI checker.
+	UI elements.
 */
-
 #define POPUP_REGISTER(f, o)  std::bind(f, o, std::placeholders::_1, std::placeholders::_2)
-using PopupWindowCallback = std::function<void(class CWindowUI*, bool)>;
+using PopupWindowCallback = std::function<void(class CWindowUI *, bool)>;
 
 struct BaseElemUI
 {
-	CWindowUI *m_pWindow;
+	CWindowUI *m_pWindow{};
 };
 
 struct PopupElemUI : BaseElemUI
 {
-	char m_aTextPopup[1024];
-	PopupWindowCallback m_pCallback;
+	char m_aTextPopup[1024]{};
+	PopupWindowCallback m_pCallback{};
 };
 
 struct MessageElemUI : BaseElemUI
 {
-	char m_aMessageText[2048];
+	char m_aMessageText[2048]{};
 };
 
+/*
+	Windows UI elements are placed between CMenus and CWindows
+	Because Teeworlds does not support a layer-by-layer UI checker.
+*/
 class CWindowController : public CComponent
 {
+	PopupElemUI *CreatePopupElement(const char *pMessage, PopupWindowCallback Callback) const;
+	MessageElemUI *CreateInformationBoxElement(float Width, const char *pMessage) const;
+	void CallbackRenderGuiPopupBox(CUIRect MainView, CWindowUI *pCurrentWindow);
+	void CallbackRenderInfoWindow(CUIRect MainView, CWindowUI *pCurrentWindow);
+
 	std::vector<BaseElemUI *> m_aElements;
 
 public:
@@ -40,26 +45,15 @@ public:
 	bool OnInput(IInput::CEvent Event) override;
 
 	// popup elements
-private:
-	PopupElemUI *CreatePopupElement(const char *pMessage, PopupWindowCallback Callback) const;
-
-public:
 	void CreatePopupBox(const char *pWindowName, float Width, const char *pMessage, PopupWindowCallback Callback, CWindowUI *pWindow);
 	void CreatePopupBox(const char *pWindowName, float Width, const char *pMessage, PopupWindowCallback Callback, bool *pDepent = nullptr);
 
 	// message elements
-private:
-	MessageElemUI *CreateInformationBoxElement(float Width, const char *pMessage) const;
-
-public:
 	void CreateInformationBox(const char *pWindowName, float Width, const char *pMessage, CWindowUI *pWindow);
 	void CreateInformationBox(const char *pWindowName, float Width, const char *pMessage, bool *pDepent = nullptr);
 
 private:
 	void Update(bool *pCursor) const;
-
-	void CallbackRenderInfoWindow(CUIRect MainView, CWindowUI *pCurrentWindow);
-	void CallbackRenderGuiPopupBox(CUIRect MainView, CWindowUI *pCurrentWindow);
 };
 
 #endif
